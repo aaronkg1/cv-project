2f7ee7e8a8f8b30615663514b08db04d52a72bcc
aaronkg1 <aaronkg1@gmail.com>
2022-05-11 20:45:20 +0100

    Added skills and strengths section

--- src/Components/DisplayCV.js
+++ src/Components/DisplayCV.js
@@ -10,7 +10,7 @@ class DisplayCV extends React.Component {
   }
 
   render() {
-    const { user, jobs, education } = this.props.data;
+    const { user, jobs, education, skills } = this.props.data;
 
     return (
       <div className="half-page cv-container">
@@ -20,6 +20,10 @@ class DisplayCV extends React.Component {
         <div className="contact">
           <h1>Contact</h1>
           <DisplayCard data={user} title="user" />
+        </div>
+        <div className="about-me-section">
+          <h1>About Me</h1>
+          <p className="about-me">{user.introduction}</p>
         </div>
         <div className="jobs">
           <h1>Experience</h1>
@@ -31,6 +35,12 @@ class DisplayCV extends React.Component {
           <h1>Education</h1>
           {education.map((item) => {
             return <DisplayCard data={item} title="education" key={item.id} />;
+          })}
+        </div>
+        <div className="skills-section">
+          <h1>Skills and Strengths</h1>
+          {skills.map((skill) => {
+            return <DisplayCard data={skill} title="skill" key={skill.id} />;
           })}
         </div>
       </div>
