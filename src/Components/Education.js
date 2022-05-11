17a74803a015ef46c0fb17b6efe2df64a9b48b45
aaronkg1 <aaronkg1@gmail.com>
2022-05-11 10:47:23 +0100

    Added component for inputting education history

---
+++ src/Components/Education.js
@@ -0,0 +1,95 @@
+import React from "react";
+
+class Education extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...this.props };
+  }
+  render() {
+    const { school } = this.props;
+
+    return (
+      <div className="education form-section">
+        <label htmlFor="establishent">Establishment: </label>
+        <input
+          type="text"
+          id="establishment"
+          name="establishment"
+          value={school.establishment}
+          onChange={(e) => {
+            this.props.updateEducation(e, school.id);
+          }}
+        />
+        <label htmlFor="city">City: </label>
+        <input
+          type="text"
+          id="city"
+          name="city"
+          value={school.city}
+          onChange={(e) => {
+            this.props.updateEducation(e, school.id);
+          }}
+        />
+        <label htmlFor="qualification">Qualification: </label>
+        <input
+          type="text"
+          id="qualification"
+          name="qualification"
+          value={school.qualification}
+          onChange={(e) => {
+            this.props.updateEducation(e, school.id);
+          }}
+        />
+        <label htmlFor="subject">Subject: </label>
+        <input
+          type="text"
+          id="subject"
+          name="subject"
+          value={school.subject}
+          onChange={(e) => {
+            this.props.updateEducation(e, school.id);
+          }}
+        />
+        <label htmlFor="dateFrom">From: </label>
+        <input
+          type="date"
+          id="dateFrom"
+          name="dateFrom"
+          value={school.dateFrom}
+          onChange={(e) => {
+            this.props.updateJob(e, school.id);
+          }}
+        />
+        <label htmlFor="dateTo">To: </label>
+        <input
+          type="date"
+          id="dateTo"
+          name="dateTo"
+          value={school.dateTo}
+          onChange={(e) => {
+            this.props.updateJob(e, school.id);
+          }}
+        />
+        <label htmlFor="notes">Notes: </label>
+        <textarea
+          id="notes"
+          name="notes"
+          value={school.notes}
+          onChange={(e) => {
+            this.props.updateEducation(e, school.id);
+          }}
+        />
+
+        <button
+          onClick={(e) => {
+            this.props.deleteEducation(e, school.id);
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Education;
