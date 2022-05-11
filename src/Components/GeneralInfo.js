1a3d511fc9d06f88be6def7a573e1c51c254d38f
aaronkg1 <aaronkg1@gmail.com>
2022-05-11 10:12:26 +0100

    Added component for inputting general contact info

---
+++ src/Components/GeneralInfo.js
@@ -0,0 +1,96 @@
+import React from "react";
+import "../styles/forms.css";
+
+class GeneralInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...this.props.user,
+    };
+  }
+
+  render() {
+    return (
+      <div className="general-info form-section" key={this.state.id}>
+        <label htmlFor="firstName">First Name: </label>
+        <input
+          type="text"
+          name="firstName"
+          onChange={(e) => {
+            this.props.receiveInfo(e);
+          }}
+          value={this.props.firstName}
+          id="firstName"
+        />
+        <label htmlFor="lastName">Last Name: </label>
+        <input
+          type="text"
+          name="lastName"
+          onChange={(e) => {
+            this.props.receiveInfo(e);
+          }}
+          value={this.props.lastName}
+          id="lastName"
+        />
+        <label htmlFor="title">Title: </label>
+        <select
+          type="text"
+          name="title"
+          onChange={(e) => {
+            this.props.receiveInfo(e);
+          }}
+          value={this.props.title}
+          id="title"
+        >
+          <option value="mr">Mr</option>
+          <option value="ms">Ms</option>
+          <option value="dr">Dr</option>
+          <option value="miss">Miss</option>
+          <option value="mrs">Mrs</option>
+        </select>
+        <label htmlFor="address">Address: </label>
+        <input
+          type="text"
+          name="address"
+          onChange={(e) => {
+            this.props.receiveInfo(e);
+          }}
+          value={this.props.address}
+          id="address"
+        />
+        <label htmlFor="telephone">Telephone: </label>
+        <input
+          type="text"
+          name="telephone"
+          onChange={(e) => {
+            this.props.receiveInfo(e);
+          }}
+          value={this.props.telephone}
+          id="telephone"
+        />
+        <label htmlFor="email">Email: </label>
+        <input
+          type="text"
+          name="email"
+          onChange={(e) => {
+            this.props.receiveInfo(e);
+          }}
+          value={this.props.email}
+          id="email"
+        />
+        <label htmlFor="introduction">Introduction: </label>
+        <input
+          type="text"
+          name="introduction"
+          onChange={(e) => {
+            this.props.receiveInfo(e);
+          }}
+          value={this.props.introduction}
+          id="introduction"
+        />
+      </div>
+    );
+  }
+}
+
+export default GeneralInfo;
