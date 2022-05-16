a8fedae3517a11f96faec58ea60b9b0e45a742da
aaronkg1 <aaronkg1@gmail.com>
2022-05-16 10:14:16 +0100

    Refactored from class to functional component

--- src/Components/GeneralInfo.js
+++ src/Components/GeneralInfo.js
@@ -1,125 +1,116 @@
 import React from "react";
 import "../styles/forms.css";
 
-class GeneralInfo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ...this.props.user,
-    };
-  }
-
-  render() {
-    return (
-      <div className="general-info form-section" key={this.state.id}>
-        <label className="form-value" htmlFor="firstName">
-          First Name:{" "}
+const GeneralInfo = (props) => {
+  const { user } = props;
+  return (
+    <div className="general-info form-section" key={user.id}>
+      <label className="form-value" htmlFor="firstName">
+        First Name:{" "}
+      </label>
+      <input
+        type="text"
+        name="firstName"
+        onChange={(e) => {
+          props.receiveInfo(e);
+        }}
+        value={user.firstName}
+        id="firstName"
+      />
+      <label className="form-value" htmlFor="lastName">
+        Last Name:{" "}
+      </label>
+      <input
+        type="text"
+        name="lastName"
+        onChange={(e) => {
+          props.receiveInfo(e);
+        }}
+        value={user.lastName}
+        id="lastName"
+      />
+      <div className="photo-upload">
+        <label className="form-value" htmlFor="picture-upload">
+          Profile Picture:
         </label>
         <input
-          type="text"
-          name="firstName"
-          onChange={(e) => {
-            this.props.receiveInfo(e);
-          }}
-          value={this.props.firstName}
-          id="firstName"
+          className="file-upload"
+          type="file"
+          onChange={props.onFileChange}
+          name="profilePic"
         />
-        <label className="form-value" htmlFor="lastName">
-          Last Name:{" "}
-        </label>
-        <input
-          type="text"
-          name="lastName"
-          onChange={(e) => {
-            this.props.receiveInfo(e);
-          }}
-          value={this.props.lastName}
-          id="lastName"
-        />
-        <div className="photo-upload">
-          <label className="form-value" htmlFor="picture-upload">
-            Profile Picture:
-          </label>
-          <input
-            className="file-upload"
-            type="file"
-            onChange={this.props.onFileChange}
-            name="profilePic"
-          />
-          <i
-            className="fa-solid fa-circle-plus add-button"
-            onClick={this.props.onFileUpload}
-          ></i>
-        </div>
-        <label className="form-value" htmlFor="title">
-          Title:{" "}
-        </label>
-        <select
-          type="text"
-          name="title"
-          onChange={(e) => {
-            this.props.receiveInfo(e);
-          }}
-          value={this.props.title}
-          id="title"
-        >
-          <option value="mr">Mr</option>
-          <option value="ms">Ms</option>
-          <option value="dr">Dr</option>
-          <option value="miss">Miss</option>
-          <option value="mrs">Mrs</option>
-        </select>
-        <label className="form-value" htmlFor="address">
-          Address:{" "}
-        </label>
-        <input
-          type="text"
-          name="address"
-          onChange={(e) => {
-            this.props.receiveInfo(e);
-          }}
-          value={this.props.address}
-          id="address"
-        />
-        <label className="form-value" htmlFor="telephone">
-          Telephone:{" "}
-        </label>
-        <input
-          type="text"
-          name="telephone"
-          onChange={(e) => {
-            this.props.receiveInfo(e);
-          }}
-          value={this.props.telephone}
-          id="telephone"
-        />
-        <label className="form-value" htmlFor="email">
-          Email:{" "}
-        </label>
-        <input
-          type="text"
-          name="email"
-          onChange={(e) => {
-            this.props.receiveInfo(e);
-          }}
-          value={this.props.email}
-          id="email"
-        />
-        <label className="form-value" htmlFor="introduction">
-          Introduction:{" "}
-        </label>
-        <textarea
-          name="introduction"
-          rows="5"
-          onChange={(e) => {
-            this.props.receiveInfo(e);
-          }}
-          value={this.props.introduction}
-          id="introduction"
-        />
+        <i
+          className="fa-solid fa-circle-plus add-button"
+          onClick={props.onFileUpload}
+        ></i>
       </div>
-    );
-  }
-}
-
+      <label className="form-value" htmlFor="title">
+        Title:{" "}
+      </label>
+      <select
+        type="text"
+        name="title"
+        onChange={(e) => {
+          props.receiveInfo(e);
+        }}
+        value={user.title}
+        id="title"
+      >
+        <option value="mr">Mr</option>
+        <option value="ms">Ms</option>
+        <option value="dr">Dr</option>
+        <option value="miss">Miss</option>
+        <option value="mrs">Mrs</option>
+      </select>
+      <label className="form-value" htmlFor="address">
+        Address:{" "}
+      </label>
+      <input
+        type="text"
+        name="address"
+        onChange={(e) => {
+          props.receiveInfo(e);
+        }}
+        value={user.address}
+        id="address"
+      />
+      <label className="form-value" htmlFor="telephone">
+        Telephone:{" "}
+      </label>
+      <input
+        type="text"
+        name="telephone"
+        onChange={(e) => {
+          props.receiveInfo(e);
+        }}
+        value={user.telephone}
+        id="telephone"
+      />
+      <label className="form-value" htmlFor="email">
+        Email:{" "}
+      </label>
+      <input
+        type="text"
+        name="email"
+        onChange={(e) => {
+          props.receiveInfo(e);
+        }}
+        value={user.email}
+        id="email"
+      />
+      <label className="form-value" htmlFor="introduction">
+        Introduction:{" "}
+      </label>
+      <textarea
+        name="introduction"
+        rows="5"
+        onChange={(e) => {
+          props.receiveInfo(e);
+        }}
+        value={user.introduction}
+        id="introduction"
+      />
+    </div>
+  );
+};
 export default GeneralInfo;
